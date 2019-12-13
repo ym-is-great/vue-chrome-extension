deffefb02c6b4752c668860d592eb0d2bee39566
cym <yatmingtsai@foxmail.com>
2019-12-13 23:34:10 +0800

    fix bug

--- src/webpack-utils/manifest-loader.js
+++ src/webpack-utils/manifest-loader.js
@@ -7,7 +7,7 @@
 module.exports = function(source) {
   const manifest = JSON.parse(source)
   if (process.env.NODE_ENV !== 'production') {
-    manifest.content_security_policy = `content_security_policy": "script-src 'self' 'unsafe-eval'; object-src 'self'`
+    manifest.content_security_policy = `script-src 'self' 'unsafe-eval'; object-src 'self'`
   }
   this.emitFile('manifest.json', JSON.stringify(manifest, null, 2))
   return source
